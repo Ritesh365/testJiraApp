c995e093e803f6b94159122c08e97172c5ab308f
Ritesh Kumar <singhritesh365@gmail.com>
2024-10-04 18:36:49 +0530

    initial commit

---
+++ src/main/java/com/telus/testGitAPI/TestGitApiApplication.java
@@ -0,0 +1,13 @@
+package com.telus.testGitAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telus/testGitAPI/TestGitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telus.testGitAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
